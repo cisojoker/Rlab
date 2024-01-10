6a6fccd7503e3d66ca5ca1bf168fc28cfc61d66a
Madhur Verma <103921455+cisojoker@users.noreply.github.com>
2024-01-10 10:12:42 +0530

    Update code9.r

--- code9.r
+++ code9.r
@@ -1,32 +1,3 @@
-# You are a data analyst tasked with analyzing and visualizing a dataset.
-# The dataset contains information about student performance in a
-# course. You need to create a program that generates various types
-# of plots to help understand and present the data effectively. Write a
-# program that performs data analysis and generates visualizations for
-# a given dataset.
-# The program should:
-# • Load the necessary libraries (ggplot2).
-# • Prepare example data with columnsforstudent names,scores,
-# and attendance percentages.
-# Perform the following tasks:
-# [1] Create a scatter plot to visualize the relationship between
-# scores and attendancepercentages.
-# [2] Generate a bar plot to show the distribution of scores among
-# different students.
-# [3] Create a line plot to display the trend of scores over time
-# (assuming different students' scores were collected at
-# different time intervals).
-# [4] Generate a histogram to visualize the distribution of scores.
-# • Customize the appearance of each plot, such as color, labels,
-# and titles.
-# • Arrange the plots in a way that they are easy to compare and
-# understand.
-
-# • Provide appropriate titles for each plot and the axes.
-
-# Note: The student data created for visualization should be uniquely
-# created with respect to the number of students, names , scores,
-# column names by each one of you.
 library(ggplot2)
 library(gridExtra)
 set.seed(123)
