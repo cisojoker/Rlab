0d4ad7794058c0a28059ae474bc368b614299316
Madhur Verma <103921455+cisojoker@users.noreply.github.com>
2024-01-29 21:04:52 +0530

    Update code3.r

--- code3.r
+++ code3.r
@@ -1,23 +1,21 @@
-calfine<-function(daysoverdue){
-    if(daysoverdue<=7){
-        fine<-0
-    }else if(daysoverdue<=30){
-        finepd<-2
-        fine<-(daysoverdue-7)*finepd
-    } else{
-        fine<-400
-    }
-    return (fine)
+fineamt<-function(days){
+  if(days<=7){
+    fine<-0
+  }else if(days<=30){
+    fine<-2*days
+  }else{
+    fine<-1000
+  }
+  return (fine)
 }
-# daysoverdue<-as.integer(readline("enter the number of days")
-daysoverdue<-50
-fm<-calfine(daysoverdue)
-if(fm==0){
-    cat("no fine")
+days<-as.integer(readline("enter the number of days"))
+val<-fineamt(days)
+if(val==0){
+  cat("no fine")
 }else{
-    if(daysoverdue<=30){
-        cat("pay the fine %d",fm)
-    }else{
-        cat("you exhausted the limit")
-    }
+  if(days<=30){
+    cat("fine is ",val,"/n")
+  }else{
+    cat("reached the cap\n")
+  }
 }
